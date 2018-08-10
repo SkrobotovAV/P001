86b65f2d33c30c036ec45427c07711936a2e721b
Andrey <as1012923@mail.ru>
2018-08-11 00:44:37 +0700

    +PointDTOtest

---
+++ common/src/test/java/jp/PointDTOtest.java
@@ -0,0 +1,23 @@
+package jp;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PointDTOtest {
+
+    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\"}";
+    private String autoId = "o567gfd";
+
+    @Test
+    public void toJson() throws Exception {
+        PointDTO point = new PointDTO();
+        point.setLat(56);
+        point.setLon(74);
+        point.setAutoId("o567gfd");
+        assertTrue(point.toJson().contains("\"lat\":56"));
+        System.out.println(point.toJson());
+    }
+}
